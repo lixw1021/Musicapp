91061a4f4b57f0ccd9014cdfcd5dda1a29964226
xianwei li <xil148@pitt.edu>
2017-08-07 14:16:50 -0400

    reformat

--- app/src/main/java/com/xianwei/musicapp/FavoriteActivity.java
+++ app/src/main/java/com/xianwei/musicapp/FavoriteActivity.java
@@ -16,6 +16,7 @@ public class FavoriteActivity extends AppCompatActivity {
     TextView buyIcon;
     @BindView(R.id.favorite_playing_ic)
     ImageView PlayingIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
